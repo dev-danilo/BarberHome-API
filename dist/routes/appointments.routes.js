894057b00f8ce6e4f94ca57b3ae56c258f76aa3d
Danilo Pereira <danilopx1@gmail.com>
2020-06-24 16:59:56 -0300

    listando agendamentos

--- dist/routes/appointments.routes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var appointmentsRouter = express_1.Router();
-exports.default = appointmentsRouter;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,16 +3,22 @@ import {startOfHour, parseISO} from 'date-fns'
 import AppointmentsRepository from '../repositories/AppointmentsRepository'
 
 const appointmentsRouter = Router();
-
 const appointmentsRepository = new AppointmentsRepository()
 
+
+appointmentsRouter.get('/', (request, response)=> {
+    const appointments  = appointmentsRepository.all()
+
+    return response.json(appointments)
+})
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date} = request.body;
 
     const parsedDate = startOfHour(parseISO(date))
     
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate)
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(
+        parsedDate)
 
     if(findAppointmentInSameDate){
         return response
