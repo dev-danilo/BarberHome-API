df777bb5a3d1b678546994e0d46fe2351c420db1
Danilo Pereira <danilopx1@gmail.com>
2020-06-28 19:03:41 -0300

    Atualizando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 // Rota: receber a requisição, chamar outro arquivo, devolver a resposta
 
@@ -24,5 +28,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+); // rota autenticada
 
 export default usersRouter;
