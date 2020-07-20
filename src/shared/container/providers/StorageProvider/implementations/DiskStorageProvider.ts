92f328a045a6550cd3c4e3e72715324196ef219d
Danilo Pereira <danilopx1@gmail.com>
2020-07-20 11:58:45 -0300

    upload de arquivos Amazon S3

--- src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import uploadConfig from '@config/upload';
 import IStorageProvider from '../models/IStorageProvider';
 
-class DisckStorageProvider implements IStorageProvider {
+class DiskStorageProvider implements IStorageProvider {
   public async saveFile(file: string): Promise<string> {
     await fs.promises.rename(
       path.resolve(uploadConfig.tmpFolder, file),
@@ -24,4 +24,4 @@ class DisckStorageProvider implements IStorageProvider {
   }
 }
 
-export default DisckStorageProvider;
+export default DiskStorageProvider;
